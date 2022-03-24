d6f27f5bd84d14b7cdad47f31a476d8067f7afc2
Tatsuya Kurebayashi <tkurebay@redhat.com>
2022-03-24 04:10:50 +0000

    Update lab-creation/install.sh, lab-creation/lab-manifests/sso.yaml

--- lab-creation/install.sh
+++ lab-creation/install.sh
@@ -1,24 +1,24 @@
 oc new-project lab-infra
 oc new-project crw
+oc new-project istio-system
 oc project lab-infra
 
 # Gitea Operator 用のカタログ
 oc apply -f https://raw.githubusercontent.com/redhat-gpte-devopsautomation/gitea-operator/master/catalog_source.yaml
 
 # Operator Install
-oc apply -f lab-manifests/operator.yaml -n lab-infra
+oc apply -f lab-manifests/operator.yaml
 oc apply -f lab-manifests/crw-operator.yaml -n crw
 
 # 各リソース作成 (CRWとSSOは競合するようなので分ける)
 oc apply -f lab-manifests/sso.yaml -n lab-infra
 oc apply -f lab-manifests/crw.yaml -n crw
 oc apply -f lab-manifests/gitea.yaml -n lab-infra
-oc apply -f lab-manifests/nexus-repo.yaml
-oc expose service lab-nexusrepo
+oc apply -f lab-manifests/nexus-repo.yaml -n lab-infra
 
 # モノリスアプリケーションは事前にビルド
 oc new-build php:7.4-ubi8~https://gitlab.com/openshift-starter-kit/msa-app.git --context-dir='monolith/' --name='monolith-ui' -n lab-infra
-oc new-build java:8~https://gitlab.com/openshift-starter-kit/msa-app.git --context-dir='monolith/' --name='monolith-service' -e MAVEN_MIRROR_URL=http://lab-nexusrepo-sonatype-nexus-service.lab-infra.svc/repository/maven-public/ -n lab-infra
+oc new-build java:8~https://gitlab.com/openshift-starter-kit/msa-app.git --context-dir='monolith/' --name='monolith-service' -n lab-infra
 
 # 各ユーザーからlab-infraのイメージをpullできるように
 oc policy add-role-to-group  system:image-puller system:serviceaccounts:user1-monolith -n lab-infra
@@ -38,4 +38,35 @@ oc policy add-role-to-group  system:image-puller system:serviceaccounts:user14-m
 oc policy add-role-to-group  system:image-puller system:serviceaccounts:user15-monolith -n lab-infra
 oc policy add-role-to-group  system:image-puller system:serviceaccounts:user16-monolith -n lab-infra
 
-oc get route keycloak -n lab-infra
+# OpenShift oAuthを設定
+
+ISSURE_URL=https://`oc get route keycloak -n lab-infra -o jsonpath='{.spec.host}'`/auth/realms/openshift
+
+oc create secret generic idp-secret --from-literal=clientSecret=openshift-client-secret -n openshift-config
+
+cat << EOF | oc apply -f -
+apiVersion: config.openshift.io/v1
+kind: OAuth
+metadata:
+  name: cluster
+spec:
+  identityProviders:
+  - name: lab-login 
+    mappingMethod: claim 
+    type: OpenID
+    openID:
+      clientID: openshift
+      clientSecret: 
+        name: idp-secret
+      claims: 
+        preferredUsername:
+        - preferred_username
+        name:
+        - name
+        email:
+        - email
+      issuer: $ISSURE_URL
+EOF
+
+# lab-admin に cluster-admin を付与
+oc adm policy add-cluster-role-to-user cluster-admin lab-admin
